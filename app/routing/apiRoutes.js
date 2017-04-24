f562c7c3d382dc41a8410da033c34d6109d73c7b
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-04-24 17:22:16 -0400

    new changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,19 @@ module.exports = function(app){
     });
     app.post("/api/friends", function(req, res){
       var newSurvey = req.body;
-      console.log(newSurvey);
+      for(var i = 0; i < friends.length; i++){
+        console.log(friends[i].scores);
+        var userScores = friends[i].scores;
+
+        totalDifference = 0;
+        for (var j = 0; j < friends[i].scores[j]; j++) {
+          totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+          console.log("use score "+ userScores[j]);
+          console.log("each score " + friends[i].scores[j])
+      }
+    }
+
+      console.log("total diff " + totalDifference);
       friends.push(newSurvey);
       res.json(newSurvey);
     });
